d01108892db7647b44a0d3a96d28eba17345ce1c
leewensim <alee229@calpoly.edu>
2017-11-27 04:19:49 -0800

    Added new properties to the Course.java class

--- src/logic/Course.java
+++ src/logic/Course.java
@@ -1,20 +1,27 @@
 package logic;
+//This is a testing class that should be deleted once it's appended with the current Course.java.
+//It contains all the new properties added to the class.
 
 public class Course {
 	String name;
 	int section;
-	//String days;
+	String days;
 	
-	public Course(String name, int section) {
+	public Course(String name, int section, String daysofweek) {
 		this.name = name;
 		this.section = section;
-		//this.days = daysofweek;
+		this.days = daysofweek;
 		//A string containing a single letter for each day of the week the class lands on.
 		//MON = M, TUES = T, WED = W, THURS = R, FRI = F
+	}
+	public boolean onDay(String day)
+	{
+		//Checks if the class is scheduled for a provided day.
+		return days.contains(day);
 	}
 	public String toString()
 	{
 		//Implementing a print function for testing
-		return name+"-"+Integer.toString(section);
+		return name+"-"+Integer.toString(section)+" ("+days+")";
 	}
 }
