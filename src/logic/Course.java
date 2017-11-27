f54e595555cbea0649817dea55eb4be447fea254
leewensim <alee229@calpoly.edu>
2017-11-27 03:58:40 -0800

    Commented out the days property in Course.java
    @nfayazov I'll leave the day property commented out until you get the
    chance to take a look.

--- src/logic/Course.java
+++ src/logic/Course.java
@@ -3,12 +3,12 @@ package logic;
 public class Course {
 	String name;
 	int section;
-	String days;
+	//String days;
 	
-	public Course(String name, int section, String daysofweek) {
+	public Course(String name, int section) {
 		this.name = name;
 		this.section = section;
-		this.days = daysofweek;
+		//this.days = daysofweek;
 		//A string containing a single letter for each day of the week the class lands on.
 		//MON = M, TUES = T, WED = W, THURS = R, FRI = F
 	}
