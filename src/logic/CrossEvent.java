e1b7e2957cc7417075f567faff78923a7a7cee99
sudo-smith <sseibertvonfock@gmail.com>
2017-11-29 14:29:45 -0800

    [#34] Got Sonarqube to run

---
+++ src/logic/CrossEvent.java
@@ -0,0 +1,13 @@
+package logic;
+
+import javafx.event.Event;
+import javafx.event.EventType;
+
+public class CrossEvent extends Event {
+
+	public CrossEvent(EventType<? extends Event> eventType) {
+		super(eventType);
+		// TODO Auto-generated constructor stub
+	}
+
+}
