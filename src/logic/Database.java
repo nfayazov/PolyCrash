6cf3dbeee795b342011bc3683107afd2a6a62203
sudo-smith <sseibertvonfock@gmail.com>
2017-11-30 12:56:02 -0800

    added test username and password for testing

--- src/logic/Database.java
+++ src/logic/Database.java
@@ -65,7 +65,12 @@ public class Database {
 				addSchedule(student);
 				
 				studentDb.put(student.username, student);
-			}	
+			}
+			Student student = new Student("a", "a");
+			addSchedule(student);
+			System.out.println(student.username);
+			System.out.println(student.getPassword());
+			studentDb.put(student.username, student);
 			
 			fsc.close();
 			lsc.close();
